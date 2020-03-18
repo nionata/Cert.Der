3beef41e69860e9d756f69dd00713bac971a5f4f
Paul Le Grand <paul@paullegrand.org>
2020-03-18 12:07:03 -0400

    Init front end

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Cert.Der
-## Mini-Project 1 - Web Exploits
-### CIS 4930 - Software Security 
 
-#### Structure 
-- cert.der-web: front end 
+## Mini-Project 1 - Web Exploits
+
+### CIS 4930 - Software Security
+
+#### Structure
+
+- cert.der-web: front end
 - cert.der-server: back end
+
+### Setup
+
+- To run front end, `cd cert.der-web` and run `npm run serve`
---
+++ cert.der-web/src/components/post.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="hello">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <!-- Profile pic goes here -->
+                </div>
+                <div class="col-md-10">
+                    <!-- Body goes here -->
+                    {{ msg }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Post',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    /* CSS Goes here */
+</style>
