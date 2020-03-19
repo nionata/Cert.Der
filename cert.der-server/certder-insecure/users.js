1f6ba72bbf29647ecd0af8a3f4e56840be72e87c
nionata <n.ionata129@gmail.com>
2020-03-19 16:59:38 -0400

    implemented getAll and get users

--- cert.der-server/certder-insecure/users.js
+++ cert.der-server/certder-insecure/users.js
@@ -1,11 +1,34 @@
 'use strict';
 
+const cloudsql = require('./cloudsql');
+
 exports.getAll = async () => {
-    return {'users': []};
+    try {
+        const db = await cloudsql();
+        const res = await db.query('SELECT ID, Username, Admin, ProfilePic FROM Users');
+
+        return { message: '', users: res }
+    } catch (err) {
+        throw err;
+    }
 }
 
 exports.get = async (path) => {
-    return {'user': path};
+    const splitPath = path.split('/')
+    if (splitPath.length !== 2) return { messsage: 'invalid user path' }
+
+    const id = parseInt(splitPath[1])
+    if (id === NaN) return { message: 'invalid user id' }
+
+    try {
+        const db = await cloudsql();
+        const res = await db.query('SELECT ID, Username, Admin, ProfilePic FROM Users WHERE ID = ?', [id]);
+        if (!res || !res.length) return { message: 'user not found' };
+
+        return { message: '', user: res };
+    } catch (err) {
+        throw err;
+    }
 }
 
 exports.updateImage = async (path, body) => {
