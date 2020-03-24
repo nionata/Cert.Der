3b380278ac7848b84c9da7940222e64b0293217c
Paul Le Grand <paul@paullegrand.org>
2020-03-23 23:13:46 -0400

    Update cloudsql connection

--- cert.der-server/certder-secure/utils/cloudsql.js
+++ cert.der-server/certder-secure/utils/cloudsql.js
@@ -5,8 +5,10 @@ const { DB_USER, DB_PASS, DB_NAME, CLOUD_SQL_CONNECTION_NAME } = process.env
 
 module.exports = () => mysql.createPool({
     user: DB_USER,
-    password: DB_PASS, 
+    password: DB_PASS,
     database: DB_NAME,
+    charset: 'utf8mb4',
+    autocommit: true,
     socketPath: `/cloudsql/${CLOUD_SQL_CONNECTION_NAME}`,
-    connectionLimit: 1,
+    connectionLimit: 3,
 })
